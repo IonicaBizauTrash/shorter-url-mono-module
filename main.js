6b34010b9d2a4e67e66f102e13a7e2f6f2a86d99
Ionica Bizau <bizauionica@yahoo.com>
2013-05-26 15:49:49 +0300

    Added alerts.

--- main.js
+++ main.js
@@ -1,31 +1,27 @@
-"use strict";
-
-define(["/jquery.js"], function () {
-
-    // On load
-    function init(config) {
-        self = this;
-        
-        // Insert a new url
-        $(".btn-insert").on("click", function() {
-            var longUrl = $("#long-url").val().trim();
-
-            self.link("insert", { "data": longUrl }, function(err, url) {
-                console.log(url);
-            });
-        });
-        
-        // Go to url
-        $(".btn-get").on("click", function() {
-            var urlCode = $("#short-url").val().trim();
-
-            self.link("get", { "data": urlCode }, function(err, url) {
-                console.log(url);
-                return;
-                window.location = url;
-            });
-        });
-    }
+module.exports = function (config) {
+    self = this;
     
-    return init;
-});
+    // Insert a new url
+    $(".btn-insert").on("click", function() {
+        var longUrl = $("#long-url").val().trim();
+
+        self.link("insert", { "data": longUrl }, function(err, url) {
+            if (err) { return alert("ERROR: " + JSON.stringify(err)); }
+
+            console.log(longUrl.length);
+
+            alert("Copy this code: " + url + "\n" + 
+                  "This will redirect you to your long url: " + 
+                  (longUrl.length > 20 ? longUrl.substring(0, 10) + "..." + longUrl.substring(longUrl.length - 5) : longUrl));
+        });
+    });
+    
+    // Go to url
+    $(".btn-get").on("click", function() {
+        var urlCode = $("#short-url").val().trim();
+
+        self.link("get", { "data": urlCode }, function(err, url) {
+            window.location = url;
+        });
+    });
+}
